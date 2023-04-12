9f1fbb13c82578b8554809e0d17b4ea55d98849f
Hoan <hoanleecr@gmail.com>
2023-04-11 18:58:54 -0700

    finished skills part, added a skill bar animation

--- script.js
+++ script.js
@@ -15,3 +15,29 @@ let typed = new Typed('.auto-input', {
     backDelay: 2000,
     loop: true,
 })
+
+
+// Skill bar Animation
+// grab the progress bars by using queryselectorall
+const skillBars = document.querySelectorAll('.inner-line')
+
+// Have the window listen to the 
+window.addEventListener('scroll', function() {
+
+    // iterate through each of the skill bar
+    skillBars.forEach(skillBar => {
+
+        // get the rectangle of the skill bar
+        const rect = skillBar.getBoundingClientRect()
+
+        // Check if skill bar is visible on the screen
+        if(rect.top < this.window.innerHeight && rect.bottom >= 0) {
+
+            // get the width of the skill bar by giving it an attribute of data-width=??
+            const width = skillBar.getAttribute('data-width')
+
+            // Now Animate the skill bar
+            skillBar.style.width = width + '%'
+        }
+    })
+})
